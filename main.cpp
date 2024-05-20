8aef0f3417a4d58e513da82b4e98b7da6c7ded99
psjung <psjung416@gmail.com>
2024-05-21 03:23:43 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
-/*void func(int a, int b) {
-	return a+b;
-}*/
-
 int main() {
     std::cout << "Hello, World!" << std::endl;
-//    std::cout << func(5, 100) << std::endl;
     return 0;
 }
