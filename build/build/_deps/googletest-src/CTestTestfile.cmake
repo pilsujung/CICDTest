1faaf82539e715c29e5af4e8431d09da527388f0
Your Name <you@example.com>
2024-07-03 18:07:36 +0900

    r

---
+++ build/build/_deps/googletest-src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/example/CICDTest/build/_deps/googletest-src
+# Build directory: C:/Users/user/Desktop/example/CICDTest/build/build/_deps/googletest-src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/build/_deps/googletest-src/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/example/CICDTest/build/_deps/googletest-src/googletest
+# Build directory: C:/Users/user/Desktop/example/CICDTest/build/build/_deps/googletest-src/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
